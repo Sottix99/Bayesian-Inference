256439452aec3808a46fa7b04768e844c7eaa1c3
Sottix99 <104276287+Sottix99@users.noreply.github.com>
2022-04-25 13:05:54 +0200

    Add files via upload

---
+++ Binomial_n_size.R
@@ -0,0 +1,36 @@
+
+# obs
+s_obs<-12
+
+# the N_Max is equal to 200
+
+# function for n
+likelihood <- function(N){
+  out <- dbinom(x=s_obs,size=N,p=0.45)
+  return(out)
+}
+
+
+format(likelihood(12), scientific=FALSE) # likelihood for N=12
+
+format(likelihood(12)* (1/200), scientific=FALSE) # it's a non-conjugate example --> likelihood*prior and also the prior is flat
+
+
+posterior<-function(n){
+  out<-likelihood(n)*(1/200)
+  return(out)
+}
+
+# mode
+which.max(posterior(1:200))
+
+# mean
+sum((1:200)*(posterior(1:200)/sum(posterior(1:200)))) # prima normalizzo la funzione col suo massimo e poi la moltiplico per 1:200
+
+# median 
+(1:200)*(cumsum(posterior(1:200)/sum(posterior(1:200)))>=0.5) # ----> 27
+
+
+
+
+  
